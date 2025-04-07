091a9f1527cd9c81ea49c9cc2ae6df0ecd5e24b5
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2025-04-06 23:45:23 -0500

    Update base href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2396213622"
+    serviceWorkerVersion: "3011203779"
   }
 });
