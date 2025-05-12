7cdac162664e87ae55a6d66815ca548bd3975787
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2025-05-12 16:35:59 -0500

    Update code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1694031058"
+    serviceWorkerVersion: "2492035401"
   }
 });
